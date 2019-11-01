8a16b90ef46385561a693526143ed5d85de063a5
huyenthanh09 <huyennguyen@gooddata.com>
2019-11-01 15:09:36 +0700

    first commit

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // module.exports is important for setupProxy.js
 module.exports = {
     appName: "My App",
-    backend: "https://secure.gooddata.com",
-    projectId: "snajpgn5ooefvxe8hlx5plpuuuoitmnb",
+    backend: "https://staging3.intgdc.com",
+    projectId: "ht3owbpk6h0yfjtkcsgva3osu3z7paol",
 };
